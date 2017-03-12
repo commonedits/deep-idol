3dd62d38616eb7d2edcc2f7d38e262828baa6ba6
Tosin <midastouchproductions@gmail.com>
2017-03-12 11:00:21 -0500

    create components

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import './App.css';
 import {Route, BrowserRouter as Router} from 'react-router-dom';
 
 import HomePage from './HomePage/HomePage'
+import Nav from './Nav/Nav'
 
 class App extends Component {
   render() {
     return (
      <div>
+      {/* Nav Bar */}
       <Nav />
+      <div className='container'>
+
+      {/* Insert pages into this router */}
       <Router>
        <Route path='/' component={HomePage} />
       </Router>
+     </div>
      </div>
     );
   }
